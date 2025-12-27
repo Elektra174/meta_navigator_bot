c002e0214d584e38630c3bfbd6e60351c52b5b96
Elektra174 <elektra-174@yandex.ru>
2025-12-27 12:05:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ CHANNEL_ID = "@metaformula_life"
 # Прямые ссылки на изображения
 LOGO_START_URL = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/logo11.png"
 LOGO_AUDIT_URL = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/logo.png.png"
-GUIDE_URL = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/revizia_guide.pdf"
+GUIDE_URL = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/guide.pdf"
 
 client = Cerebras(api_key=CEREBRAS_API_KEY)
 bot = Bot(token=TOKEN)
