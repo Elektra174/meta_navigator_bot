6c5bb99879924075049b459acd7fce99a5a8796d
Elektra174 <elektra-174@yandex.ru>
2026-01-08 16:21:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ except ImportError:
 # Ресурсы проекта
 ССЫЛКА_ЛОГО_ФОРМУЛА = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/logo.png"
 ССЫЛКА_ЛОГО_НАВИГАТОР = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot/main/logo11.png"
-ССЫЛКА_ПРОТОКОЛА = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot-TEST/main/avtopilot.pdf"
+ССЫЛКА_ПРОТОКОЛА = "https://raw.githubusercontent.com/Elektra174/meta_navigator_bot-TEST/main/Autopilot_System_Protocol.pdf"
 ИМЯ_ФАЙЛА_ПРОТОКОЛА = "ДЕШИФРОВКА_АВТОПИЛОТА.pdf"
 ССЫЛКА_МАСТЕРКЛАССА = "https://youtube.com/playlist?list=PLyour_playlist_id"
 ССЫЛКА_КАНАЛА = "https://t.me/metaformula_life"
